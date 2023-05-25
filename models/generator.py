4ce408f9dd73a2944b4a24dd85bc06a67bd1b70c
Daan Rongen <daanrongen@hotmail.com>
2023-05-25 16:41:42 +0100

    undo 128x128 upscale

--- models/generator.py
+++ models/generator.py
@@ -6,29 +6,25 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(noise_dim, features * 16, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(features * 16),
-            nn.ReLU(True),
-            # state size. (ngf*16) x 4 x 4
-            nn.ConvTranspose2d(features * 16, features * 8, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(noise_dim, features * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(features * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 8 x 8
+            # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(features * 8, features * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(features * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 16 x 16
+            # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d(features * 4, features * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(features * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 32 x 32
+            # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(features * 2, features, 4, 2, 1, bias=False),
             nn.BatchNorm2d(features),
             nn.ReLU(True),
-            # state size. (ngf) x 64 x 64
+            # state size. (ngf) x 32 x 32
             nn.ConvTranspose2d(features, img_channels, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (nc) x 128 x 128
+            # state size. (nc) x 64 x 64
         )
 
     def forward(self, input):
